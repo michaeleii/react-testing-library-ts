1972d9c49a56a52a9c59317d706f37552b68d5d3
Michael Lei <michaelleie0ca@gmail.com>
2023-04-30 11:46:53 -0700

    Edit header test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,6 @@ export default function Header({ title }: { title: string }) {
 			<h1 title="Header" className="header">
 				{title}
 			</h1>
-			{/* <h3 data-testid="header-2" className="header">Hello</h3> */}
 		</>
 	);
 }
