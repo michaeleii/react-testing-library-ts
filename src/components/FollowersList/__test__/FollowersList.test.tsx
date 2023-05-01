c60b3e7b5a02e6b7abe327b428eac0766f0b6ea3
Michael Lei <michaelleie0ca@gmail.com>
2023-04-30 22:30:17 -0700

    edit

--- src/components/FollowersList/__test__/FollowersList.test.tsx
+++ src/components/FollowersList/__test__/FollowersList.test.tsx
@@ -13,21 +13,8 @@ const MockFollowersList = () => {
 
 describe("FollowersList", () => {
 	beforeEach(() => {
-		// console.log("RUNS BEFORE EACH TEST")
 		vi.mock("../../../__mocks__/axios");
 	});
-
-	// beforeAll(() => {
-	//     console.log("RUNS ONCE BEFORE ALL TESTS")
-	// })
-
-	// afterEach(() => {
-	//     console.log("RUNS AFTER EACH TEST")
-	// })
-
-	// afterAll(() => {
-	//     console.log("RUNS ONCE AFTER ALL TESTS")
-	// })
 
 	it("should fetch and render input element", async () => {
 		render(<MockFollowersList />);
